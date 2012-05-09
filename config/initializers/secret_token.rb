10c1fe69b410ca151053307d36af73dbf8900577
akiii <akiii@sfc.wide.ad.jp>
2012-05-09 22:34:36 +0900

    Set a rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SfcHash::Application.config.secret_token = 'b8e4b6db4d967716b4ee4bd1e70bffa3ae7f9cbf8ace1859ea7214c49ccf48255c1f8abc68252adcf2908f214925aac52bd57fc6df1411ba851d3561dbc7d37a'
