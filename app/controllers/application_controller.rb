63d57ff1d062a3f838c4bbf268c223951d727877
akiii <akiii@sfc.wide.ad.jp>
2012-05-10 18:59:43 +0900

    Added methods to application_controller.rb .

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,34 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  private
+
+  def check_administor
+    if session[:id]
+      if User.find(session[:id]).login_name != "t10740af"
+        redirect_to :controller => "login", :action => "index"
+      end
+    end
+  end
+
+  def current_user
+    @current_user ||= User.find(session[:id]) if session[:id]
+  end
+
+  def save_student_id(student_id)
+    user = User.find(session[:id])
+    user.session_id = student_id
+    user.save
+  end
+
+  def get_student_id
+    return User.find(session[:id]).session_id
+  end
+
+  def require_login
+    unless current_user
+      redirect_to :controller => "login", :action => "index"
+    end
+  end
+
 end
