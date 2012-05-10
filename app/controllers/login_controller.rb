316aa168216bd09436ec98cb01a47ac81d508f08
akiii <akiii@sfc.wide.ad.jp>
2012-05-11 00:04:12 +0900

    Fixed a login controller.

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -5,6 +5,10 @@ class LoginController < ApplicationController
   require 'kconv'
   require 'httpclient'
 
+#  def index
+#    setup_database
+#  end
+
   def submit
     client = HTTPClient.new
     data = {"u_login" => params[:u_login], "u_pass" => params[:u_pass]}
@@ -13,6 +17,7 @@ class LoginController < ApplicationController
     puts id
     if id =~ /url=(.+?)"/
       id =~ /id=(.+)&type/
+      id = $1
 
       if User.where(:login_name => params[:u_login]).exists? then
         users = User.where(:login_name => params[:u_login])
@@ -34,6 +39,46 @@ class LoginController < ApplicationController
       flash[:error] = "ログイン名かパスワードが違います。"
       redirect_to :action => 'index'
     end
+  end
+
+  def setup_database
+=begin
+    day = Day.new
+    day.self = "月"
+    day.save
+    day = Day.new
+    day.self = "火"
+    day.save
+    day = Day.new
+    day.self = "水"
+    day.save
+    day = Day.new
+    day.self = "木"
+    day.save
+    day = Day.new
+    day.self = "金"
+    day.save
+    day = Day.new
+    day.self = "土"
+    day.save
+    day = Day.new
+    day.self = "日"
+    day.save
+
+    for i in 1..7
+      timetable = Timetable.new
+      timetable.self = i
+      timetable.save
+    end
+
+    term = Term.new
+    term.season = "春"
+    term.save
+    term = Term.new
+    term.season = "秋"
+    term.save
+=end
+
   end
 
 end
