a5aacaf02f2cdc99db77b2b19c747e3da031c49b
akiii <akiii@sfc.wide.ad.jp>
2012-05-13 02:27:15 +0900

    Added method.

--- app/models/subject.rb
+++ app/models/subject.rb
@@ -1,2 +1,12 @@
 class Subject < ActiveRecord::Base
+  has_many :name, :uniq => true
+
+  def exist(name)
+    if Subject.find_by_name(name)
+      return true
+    else
+      return false
+    end
+  end
+
 end
--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -1,2 +1,12 @@
 class Teacher < ActiveRecord::Base
+  has_many :name, :uniq => true
+
+  def exist(name)
+    if Teacher.find_by_name(name)
+      return true
+    else
+      return false
+    end
+  end
+
 end
