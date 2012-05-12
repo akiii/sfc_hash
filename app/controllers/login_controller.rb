13f0ea6f49c9fb1e852a36f227c93099b275aa47
akiii <akiii@sfc.wide.ad.jp>
2012-05-13 02:26:46 +0900

    Modified a login_controller.

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -5,10 +5,6 @@ class LoginController < ApplicationController
   require 'kconv'
   require 'httpclient'
 
-#  def index
-#    setup_database
-#  end
-
   def submit
     client = HTTPClient.new
     data = {"u_login" => params[:u_login], "u_pass" => params[:u_pass]}
@@ -39,46 +35,6 @@ class LoginController < ApplicationController
       flash[:error] = "ログイン名かパスワードが違います。"
       redirect_to :action => 'index'
     end
-  end
-
-  def setup_database
-=begin
-    day = Day.new
-    day.self = "月"
-    day.save
-    day = Day.new
-    day.self = "火"
-    day.save
-    day = Day.new
-    day.self = "水"
-    day.save
-    day = Day.new
-    day.self = "木"
-    day.save
-    day = Day.new
-    day.self = "金"
-    day.save
-    day = Day.new
-    day.self = "土"
-    day.save
-    day = Day.new
-    day.self = "日"
-    day.save
-
-    for i in 1..7
-      timetable = Timetable.new
-      timetable.self = i
-      timetable.save
-    end
-
-    term = Term.new
-    term.season = "春"
-    term.save
-    term = Term.new
-    term.season = "秋"
-    term.save
-=end
-
   end
 
 end
