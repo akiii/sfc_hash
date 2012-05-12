a96eb439ad78479861c5c918ccaa6ba4753dcbc6
akiii <akiii@sfc.wide.ad.jp>
2012-05-13 02:24:20 +0900

    Added a new model.

---
+++ db/migrate/20120512163321_create_syllabus_words.rb
@@ -0,0 +1,10 @@
+class CreateSyllabusWords < ActiveRecord::Migration
+  def change
+    create_table :syllabus_words do |t|
+      t.string :string
+      t.integer :subject_id
+
+      t.timestamps
+    end
+  end
+end
