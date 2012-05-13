9620cf796f6bdbddae8dec334e1630337550c178
akiii <akiii@sfc.wide.ad.jp>
2012-05-14 00:17:23 +0900

    Added methods.

--- app/models/subject_info.rb
+++ app/models/subject_info.rb
@@ -1,2 +1,22 @@
 class SubjectInfo < ActiveRecord::Base
+
+  def exist(term, day, timetable, subject, teacher)
+    subject = Subject.find_by_name(subject)
+    teacher = Teacher.find_by_name(teacher)
+    if subject && teacher
+      subject_info = SubjectInfo.find_by_subject_id_and_teacher_id(subject.id, teacher.id)
+      if subject_info
+        if subject_info.day_id == Day.find_by_self(day).id && subject_info.timetable_id == Timetable.find_by_self(Timetable.new.get_timetable_number_from_string(timetable)).id
+          return true
+	else
+	  return false
+	end
+      else
+        return false
+      end
+    else
+      return false
+    end
+  end
+
 end
--- app/models/timetable.rb
+++ app/models/timetable.rb
@@ -1,2 +1,22 @@
+# -*- coding: utf-8 -*-
 class Timetable < ActiveRecord::Base
+
+  def get_timetable_number_from_string(timetable_string)
+    if timetable_string == '１'
+      return 1
+    elsif timetable_string == '２'
+      return 2
+    elsif timetable_string == '３'
+      return 3
+    elsif timetable_string == '４'
+      return 4
+    elsif timetable_string == '５'
+      return 5
+    elsif timetable_string == '６'
+      return 6
+    elsif timetable_string == '７'
+      return 7
+    end
+  end
+
 end
