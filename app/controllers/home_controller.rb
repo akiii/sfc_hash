640035994d558914ce84abfb391237438860a90c
akiii <akiii@sfc.wide.ad.jp>
2012-05-14 09:44:58 +0900

    Modified home_controller.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,42 @@
 # -*- coding: utf-8 -*-
 class HomeController < ApplicationController
+  include MySubjectsInfoHelper
 
   def index
+    @subjects_info = my_subjects_info
+    puts "=============="
+    hashtags = Hashtag.all
+    hashtags.each do |hashtag|
+      puts hashtag.string
+      puts hashtag.subject_info_id
+    end
+  end
+
+  def add
+    @subject_info = SubjectInfo.find(params[:subject_info_id])
+    @room = Room.all
+  end
+
+  def submit
+    subject_info = SubjectInfo.find(params[:subject_info_id])
+    subject_info.room_id = Room.find(params[:room]).id
+    subject_info.save
+    if /^#(.+?)$/ =~ params[:hashtag][:string]
+      hashtag = Hashtag.new
+      hashtag.string = params[:hashtag][:string]
+      hashtag.subject_info_id = params[:subject_info_id]
+      hashtag.save
+    else
+      flash[:error] = "ハッシュタグが正しくありません。"
+      redirect_to :action => 'add', :subject_info_id => params[:subject_info_id]
+      return
+    end
+
+    if params[:state] == 'add'
+    elsif params[:state] == 'edit'
+    end
+
+    redirect_to :action => 'index'
   end
 
 end
