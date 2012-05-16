c73181866e577eb8d5476a8af73062ab0e430bee
akiii <akiii@sfc.wide.ad.jp>
2012-05-17 02:57:10 +0900

    Modified home_controller.rb .

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -36,10 +36,9 @@ class HomeController < ApplicationController
     subject_info.room_id = Room.find(params[:room]).id
     subject_info.save
     if /^#(.+?)$/ =~ params[:hashtag][:string]
+      hashtag = Hashtag.new
       if Hashtag.find_by_string(params[:hashtag][:string])
         hashtag = Hashtag.find_by_string(params[:hashtag][:string])
-      else
-        hashtag = Hashtag.new
       end
       hashtag.string = params[:hashtag][:string]
       hashtag.subject_info_id = params[:subject_info_id]
