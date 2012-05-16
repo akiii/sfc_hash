98ae12fce4a7fa9e00e2ae0178cded53774f790f
akiii <akiii@sfc.wide.ad.jp>
2012-05-17 02:56:51 +0900

    Modified hashtag.rb .

--- app/models/hashtag.rb
+++ app/models/hashtag.rb
@@ -2,7 +2,7 @@ class Hashtag < ActiveRecord::Base
   belongs_to :subject_info
   has_many :tweets
 
-  validates :hashtag,
+  validates :string,
     :length => { :minimum => 1, :maximum => 30 },
     :format => { :with => /^#(.+?)$/ }
 end
