fdd0001035ca4b396999e9503136ab2a9f36de97
akiii <akiii@sfc.wide.ad.jp>
2012-05-24 15:31:20 +0900

    Modified a hashtags_candidate_controller.rb .

--- app/controllers/hashtags_candidate_controller.rb
+++ app/controllers/hashtags_candidate_controller.rb
@@ -4,6 +4,7 @@ class HashtagsCandidateController < ApplicationController
   def index
     @hashtags_candidate = HashtagCandidate.all
     @hashtags_candidate = @hashtags_candidate.sort_by{ |hashtag_candidate| hashtag_candidate.point }
+    @hashtags_candidate.reverse!
     @hashtags_candidate = Kaminari.paginate_array(@hashtags_candidate).page(params[:page]).per(100)
   end
 
