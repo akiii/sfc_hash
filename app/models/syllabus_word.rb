0f9c4b962928fd2b6f8e67ad89d73c12881ce49d
akiii <akiii@sfc.wide.ad.jp>
2012-06-16 09:22:58 +0900

    Added a new method.

--- app/models/syllabus_word.rb
+++ app/models/syllabus_word.rb
@@ -11,4 +11,17 @@ class SyllabusWord < ActiveRecord::Base
     end
   end
 
+  def set_point
+    syllabus_words_count_include_string = SyllabusWord.find_all_by_subject_info_id_and_string(self.subject_info_id, self.string).count
+    syllabus_words_count = SyllabusWord.find_all_by_subject_info_id(self.subject_info_id).count
+    tf = syllabus_words_count_include_string * 1.0 / syllabus_words_count * 100
+    
+    if !$syllabus_all_count
+      $syllabus_all_count = SyllabusWord.group('subject_info_id').to_a.count
+    end
+    syllabus_count_include_string = SyllabusWord.where('string = ?', self.string).group('subject_info_id').to_a.count
+    idf = Math::log($syllabus_all_count/syllabus_count_include_string)
+    self.point = tf * idf
+  end
+
 end
